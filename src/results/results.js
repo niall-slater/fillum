4fb43028abf8d8766c70263d7fc12863f100be9d
NigeyNige <niallslater@gmail.com>
2018-04-02 21:07:00 +0100

    Search result display now working

--- src/results/results.js
+++ src/results/results.js
@@ -11,15 +11,25 @@ export class Results extends React.Component {
 	}
 	
 	render() {
-		console.log(this.props.results);
+		var title = "Title";
+		var year = "Year";
+		var poster = "https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180";
+		
+		if (this.props.results.Search !== undefined) {
+			var foundItem = this.props.results.Search[0];
+			title = foundItem.Title;
+			year = foundItem.Year;
+			poster = foundItem.Poster;
+		}
+		
 		return(
 			<div className="results">
 				<Card>
-					<CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+					<CardImg top width="100%" src={poster} alt="Card image cap" />
 					<CardBody>
-						<CardTitle>Title</CardTitle>
-						<CardSubtitle>Card subtitle</CardSubtitle>
-						<CardText>Text text text text text text text text text.</CardText>
+						<CardTitle>{title}</CardTitle>
+						<CardSubtitle>{year}</CardSubtitle>
+						<CardText>Description</CardText>
 						<Button>Button</Button>
 					</CardBody>
 				</Card>
